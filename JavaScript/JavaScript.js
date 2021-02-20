b7849c16808b90274493f6bd1aab55108cfcc05e
Aleksey <fistashki22@gmail.com>
2021-02-20 19:58:38 +0300

    Well done

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-alert(1 && 0);
-alert(1 && 5);
-
-alert(null && 5);
 alert(0 && 'No matter what');
-=======
-alert(1 && 0);
-alert(1 && 5);
-
-alert(null && 5);
-alert(0 && 'No matter what');
->>>>>>> 507d715e07bd76dbc61184372f1542ccdcfd03f2
